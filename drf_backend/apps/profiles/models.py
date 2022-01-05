686df786a0c4da8aafc3e7ff8b0129acf63a13d2
Suslov Vsevolod <v.suslov@yandex.ru>
2022-01-05 18:51:02 +0300

    created custom user model

--- drf_backend/apps/profiles/models.py
+++ drf_backend/apps/profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom user model"""
+    middle_name = models.CharField(max_length=50)
+    first_login = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=14)
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True)
--- drf_backend/config/urls.py
+++ drf_backend/config/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
